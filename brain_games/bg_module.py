73490e526f8af5834c651048942c79f115150064
Sergei Mikurov <454004@mail.ru>
2023-03-11 23:23:01 +0500

    Add a select of task for the selected game
    Change bg_module.py
    Add in function begin_the_game selection of function for task of game.
    Add an argument (maximum number) to function generate_random_number().
    Change the function name from formulate_a_task() to formulate_task_brain_even().
    Add function formulate_task_brain_calc().

--- brain_games/bg_module.py
+++ brain_games/bg_module.py
@@ -1,6 +1,6 @@
 # file:bg_module.py
 import prompt
-from random import randint
+from random import choices, randint
 
 
 def greetings():
@@ -13,13 +13,34 @@ def ask_player_name():
     return player_name
 
 
-def formulate_a_task():
-    task = randint(1, 100)
+def generate_random_number(number_max):
+    return randint(1, number_max)
+
+
+def formulate_task_brain_even():
+    task = generate_random_number()
     if task % 2 == 0:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
     return (task, correct_answer)
+
+
+def formulate_task_brain_calc():
+    first_item = generate_random_number(100)
+    mathematical_operation = choices('+-*')
+    match mathematical_operation[0]:
+        case '+':
+            second_item = generate_random_number(100)
+            correct_answer = first_item + second_item
+        case '-':
+            second_item = generate_random_number(100)
+            correct_answer = first_item - second_item
+        case '*':
+            second_item = generate_random_number(10)
+            correct_answer = first_item * second_item
+    task = f'{first_item} {mathematical_operation[0]} {second_item}'
+    return task, str(correct_answer)
 
 
 def display_the_rules(rules_of_game):
@@ -56,7 +77,11 @@ def begin_the_game(name_of_game, rules_of_game):
     display_the_rules(rules_of_game)
 
     for _ in range(count_of_questions):
-        task, correct_answer = formulate_a_task()
+        match name_of_game:
+            case 'brain_even':
+                task, correct_answer = formulate_task_brain_even()
+            case 'brain_calc':
+                task, correct_answer = formulate_task_brain_calc()
         ask_player_question(task)
         player_answer = get_player_answer()
         display_test_result(player_answer, correct_answer,
