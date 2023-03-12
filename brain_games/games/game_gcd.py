9d8714a7b6155d07ba1d313d49b754993e47ced9
Sergei Mikurov <454004@mail.ru>
2023-03-12 19:24:12 +0500

    remove sorted from formulate_task()

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -5,14 +5,15 @@ rules_of_game = 'Find the greatest common divisor of given numbers.'
 
 
 def formulate_task():
-    lower_number, largest_number = sorted((randint(1, 100), randint(1, 100)))
+    first_number, second_number = ((randint(1, 100), randint(1, 100)))
+    lesser_number = min(first_number, second_number)
 
-    for greatest_common_divisor in range(lower_number, 0, -1):
-        if lower_number % greatest_common_divisor == 0 and\
-           largest_number % greatest_common_divisor == 0:
+    for greatest_common_divisor in range(lesser_number, 0, -1):
+        if first_number % greatest_common_divisor == 0 and\
+           second_number % greatest_common_divisor == 0:
             break
 
-    task = f'{lower_number} {largest_number}'
+    task = f'{first_number} {second_number}'
     correct_answer = str(greatest_common_divisor)
 
     return task, correct_answer
