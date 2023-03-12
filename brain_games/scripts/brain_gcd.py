07091da604386665ab3650857ae9c0cf3fcf3f35
Sergei Mikurov <454004@mail.ru>
2023-03-12 18:49:48 +0500

    add brain_games/scripts/brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# file:brain_gcd.py
+import brain_games.game_logics
+
+
+def main():
+    name_of_game = 'brain_gcd'
+    brain_games.game_logics.begin_to_game(name_of_game)
+
+
+if __name__ == '__main__':
+    main()
