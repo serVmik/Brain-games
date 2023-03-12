e34ef0585d750b30a1aacc3ea23a3731a6f5db37
Sergei Mikurov <454004@mail.ru>
2023-03-12 22:15:11 +0500

    add brain_games/scripts/brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python3
+# file:brain_progression.py
+import brain_games.game_logics
+
+
+def main():
+    name_of_game = 'brain_progression'
+    brain_games.game_logics.begin_to_game(name_of_game)
+
+
+if __name__ == '__main__':
+    main()
