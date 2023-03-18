4881a0feb548e8bd917e3eafe6f930d30dfb2647
Sergei Mikurov <454004@mail.ru>
2023-03-18 15:53:50 +0500

    add the is_prime() function to game_prime.py

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -3,19 +3,22 @@ from random import randint
 RULES_OF_GAME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def create_task():
-    RANGE_OF_NUMBER = 100
-    number = randint(1, RANGE_OF_NUMBER)
-    answer = 'yes'
-
-    task = str(number)
-
+def is_prime(number):
     if number == 1:
-        answer = 'no'
-        return task, answer
+        return False
 
     for divider in range(2, round(number / 2) + 1):
         if number % divider == 0:
-            answer = 'no'
+            return False
+
+    return True
+
+
+def create_task():
+    RANGE_OF_NUMBER = 100
+    number = randint(1, RANGE_OF_NUMBER)
+
+    task = str(number)
+    answer = ('no', 'yes')[is_prime(number)]
 
     return task, answer
