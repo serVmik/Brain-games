e1419fd7540a08d2c4c50f91a1d21f5f46a5a6f0
Sergei Mikurov <454004@mail.ru>
2023-03-20 13:36:20 +0500

    rename values to constants in game_logics.py
    rename module_gg to MODULE_GEARS
    raname count_of_questions to COUNT_OF_QUESTIONS

--- brain_games/game_logics.py
+++ brain_games/game_logics.py
@@ -1,25 +1,27 @@
 import brain_games.game_gears
-module_gg = brain_games.game_gears
+MODULE_GEARS = brain_games.game_gears
 
 
+# MODULE_GAME is the game module reference
+# that the begin_to_game()function receives when it is called
 def begin_to_game(MODULE_GAME):
-    count_of_questions = 3
+    COUNT_OF_QUESTIONS = 3
     result_of_game = 'win'      # flag for congratulations
 
-    module_gg.greetings()
-    player_name = module_gg.ask_player_name()
-    module_gg.display_the_rules(MODULE_GAME.RULES_OF_GAME)
+    MODULE_GEARS.greetings()
+    player_name = MODULE_GEARS.ask_player_name()
+    MODULE_GEARS.display_the_rules(MODULE_GAME.RULES_OF_GAME)
 
-    for _ in range(count_of_questions):
+    for _ in range(COUNT_OF_QUESTIONS):
         task, correct_answer = MODULE_GAME.create_task()
-        module_gg.ask_player_question(task)
-        player_answer = module_gg.get_player_answer()
-        module_gg.display_test_result(player_answer, correct_answer,
-                                      player_name)
+        MODULE_GEARS.ask_player_question(task)
+        player_answer = MODULE_GEARS.get_player_answer()
+        MODULE_GEARS.display_test_result(player_answer, correct_answer,
+                                         player_name)
 
         if player_answer != correct_answer:
             result_of_game = 'game over'
             break
 
     if result_of_game == 'win':
-        module_gg.congratulate(player_name)
+        MODULE_GEARS.congratulate(player_name)
