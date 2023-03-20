ed4e391b6db089d00002049f54f474fc8e5525e1
Sergei Mikurov <454004@mail.ru>
2023-03-20 13:33:14 +0500

    added blank lines for readability
    added blank lines to brain_games/games/game_even.py
    added blank lines to brain_games/games/game_progression.py

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -6,8 +6,10 @@ RULES_OF_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
 def create_task():
     RANGE_OF_NUMBER = 100
     task = randint(1, RANGE_OF_NUMBER)
+
     if task % 2 == 0:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
+
     return str(task), correct_answer
--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -7,6 +7,7 @@ def create_progression():
     RANGE_OF_FIRST_NUMBER = 20
     RANGE_OF_STEP = 10
     PROGRESSION_LENGTH_RANGE = 10
+
     first_number = randint(1, RANGE_OF_FIRST_NUMBER)
     step = randint(1, RANGE_OF_STEP)
     length_of_progression = randint(5, PROGRESSION_LENGTH_RANGE)
