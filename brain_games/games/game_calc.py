c9ca64e200a97d8056d50c49603c408f2a38717d
Sergei Mikurov <454004@mail.ru>
2023-03-27 13:36:07 +0500

    fix game_calc.py
    change function choices to choice

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,4 +1,4 @@
-from random import choices, randint
+from random import choice, randint
 
 RULES_OF_GAME = 'What is the result of the expression?'
 MIN_OPERAND_VALUE = 1
@@ -8,9 +8,9 @@ MAX_OPERAND_VALUE = 100
 def create_task():
     first_operand = randint(MIN_OPERAND_VALUE, MAX_OPERAND_VALUE)
     second_operand = randint(MIN_OPERAND_VALUE, MAX_OPERAND_VALUE)
-    mathematical_operation = choices(['+', '-', '*'])
+    mathematical_operation = choice(['+', '-', '*'])
 
-    match mathematical_operation[0]:
+    match mathematical_operation:
         case '+':
             correct_answer = first_operand + second_operand
         case '-':
@@ -18,6 +18,6 @@ def create_task():
         case '*':
             correct_answer = first_operand * second_operand
 
-    task = f'{first_operand} {mathematical_operation[0]} {second_operand}'
+    task = f'{first_operand} {mathematical_operation} {second_operand}'
 
     return task, str(correct_answer)
