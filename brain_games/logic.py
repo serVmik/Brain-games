23054b9d8b528d3624880ac03ff184bd65173029
Sergei Mikurov <454004@mail.ru>
2023-03-27 13:09:37 +0500

    fix logic.py
    remove flag for congratulate
    rename module_game to module_of_game

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -3,21 +3,18 @@ from brain_games import gears
 COUNT_OF_QUESTIONS = 3
 
 
-def start_to_game(game_module):
-    result_of_game = 'win'      # flag for congratulations
+def start_to_game(module_of_game):
     gears.greetings()
     player_name = gears.ask_player_name()
-    gears.display_the_rules(game_module.RULES_OF_GAME)
+    gears.display_the_rules(module_of_game.RULES_OF_GAME)
 
     for _ in range(COUNT_OF_QUESTIONS):
-        task, correct_answer = game_module.create_task()
+        task, correct_answer = module_of_game.create_task()
         gears.ask_player_question(task)
         player_answer = gears.get_player_answer()
         gears.display_test_result(player_answer, correct_answer, player_name)
 
         if player_answer != correct_answer:
-            result_of_game = 'game over'
-            break
+            return
 
-    if result_of_game == 'win':
-        gears.congratulate(player_name)
+    gears.congratulate(player_name)
