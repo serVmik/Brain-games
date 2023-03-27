b16300ec972c7b81b7766a72d6bbed909c0244a4
Sergei Mikurov <454004@mail.ru>
2023-03-27 21:16:01 +0500

    fix module game_progression.py
        Move random values from function create_progression()
    to function creat_task().

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -8,14 +8,11 @@ MIN_PROGRESSION_LENGTH = 5
 MAX_PROGRESSION_LENGTH = 10
 
 
-def create_progression():
-    first_number = randint(MIN_VALUE_OF_NUMBER, MAX_VALUE_OF_NUMBER)
-    step = randint(MIN_VALUE_OF_NUMBER, RANGE_OF_STEP)
-    length_of_progression = randint(MIN_PROGRESSION_LENGTH,
-                                    MAX_PROGRESSION_LENGTH)
-    last_number = first_number + step * (length_of_progression - 1)
-    list_of_progression = [_ for _ in range(first_number, last_number + 1,
-                                            step)]
+def create_progression(first_number, length, step):
+    last_number_of_progression = first_number + step * (length - 1)
+    list_of_progression = [
+        str(number) for number in range(
+            first_number, last_number_of_progression + 1, step)]
 
     return list_of_progression
 
@@ -34,7 +31,15 @@ def creat_task_and_answer(progression):
 
 
 def create_task():
-    progression = create_progression()
+    first_number_of_progression = randint(
+        MIN_VALUE_OF_NUMBER, MAX_VALUE_OF_NUMBER)
+    length_of_progression = randint(
+        MIN_PROGRESSION_LENGTH, MAX_PROGRESSION_LENGTH)
+    step_of_progression = randint(
+        MIN_VALUE_OF_NUMBER, RANGE_OF_STEP)
+    progression = create_progression(
+        first_number_of_progression, length_of_progression, step_of_progression)
+
     task, correct_answer = creat_task_and_answer(progression)
 
     return task, correct_answer
