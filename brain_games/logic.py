698baeafc4cb08210487bf851389e135f5d3af44
Sergei Mikurov <454004@mail.ru>
2023-03-27 20:18:44 +0500

    fix module logic.py
       Remove import brain_games.gears.
       Move functions from module gears.py function start_to_game().

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -1,20 +1,25 @@
-from brain_games import gears
+import prompt
 
 COUNT_OF_QUESTIONS = 3
 
 
 def start_to_game(module_of_game):
-    gears.greetings()
-    player_name = gears.ask_player_name()
-    gears.display_the_rules(module_of_game.RULES_OF_GAME)
+    print('Welcome to the Brain Games!')
+    player_name = prompt.string('May I have your name? ')
+    print(f'Hello, {player_name}!')
+    print(module_of_game.RULES_OF_GAME)
 
     for _ in range(COUNT_OF_QUESTIONS):
         task, correct_answer = module_of_game.create_task()
-        gears.ask_player_question(task)
-        player_answer = gears.get_player_answer()
-        gears.display_test_result(player_answer, correct_answer, player_name)
+        print(f'Question: {task}')
+        player_answer = input('Your answer: ')
 
-        if player_answer != correct_answer:
+        if player_answer == correct_answer:
+            print('Correct!')
+        else:
+            print(f"'{player_answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'."
+                  f"Let's try again, {player_name}!")
             return
 
-    gears.congratulate(player_name)
+    print(f'Congratulations, {player_name}!')
